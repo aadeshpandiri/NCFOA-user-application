459e06bea240ef782a865bda6c416c31dc783f97
aadeshpandiri <aadeshpandiri@gmail.com>
2021-03-05 22:27:28 +0530

    05-march

--- app/src/main/java/com/example/ncfoa_user_application/ui/qr_scanner/QRScannerFragment.java
+++ app/src/main/java/com/example/ncfoa_user_application/ui/qr_scanner/QRScannerFragment.java
@@ -1,35 +1,44 @@
 package com.example.ncfoa_user_application.ui.qr_scanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 
 import com.example.ncfoa_user_application.R;
 
 public class QRScannerFragment extends Fragment {
+    Button scanbtn;
+    public static TextView scantext;
 
-    private QRScannerViewModel qrScannerViewModel;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        qrScannerViewModel =
-                new ViewModelProvider(this).get(QRScannerViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_qr_scanner, container, false);
-        final TextView textView = root.findViewById(R.id.text_qr_scanner);
-        qrScannerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View RootView = inflater.inflate(R.layout.fragment_qr_scanner, container, false);
+
+        TextView scantext=(TextView) RootView.findViewById(R.id.scantext);
+        Button scanbtn= (Button) RootView.findViewById(R.id.scanbtn);
+
+        scanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), QRScannerViewActivity.class);
+                startActivity(intent);
             }
         });
-        return root;
+
+        return RootView;
     }
+
 }
